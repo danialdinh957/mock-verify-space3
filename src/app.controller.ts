4dd0ab663920a20a0845bb0e4ef0562bef66c9bd
Danial Dinh <danialdinh@gmail.com>
2024-06-26 17:17:21 +0700

    feat: strict count times

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,20 +11,46 @@ export class AppController {
   }
 
   @Get('/accumulate')
-  accumulate(@Query('quantity') quantity: string) {
-    const nowDate = new Date().getTime();
-    const res: any = [];
+  accumulate(
+    @Query()
+    {
+      quantity,
+      startDate,
+      fromDate,
+      toDate,
+    }: {
+      quantity: string;
+      startDate: string;
+      fromDate: string;
+      toDate: string;
+    },
+  ) {
+    const startTime = new Date(startDate).getTime();
+    const fromTime = new Date(fromDate).getTime();
+    const checkpointTimes = Math.floor((fromTime - startTime) / 1000);
 
-    if (!quantity) {
+    const res: { completedTimes: number; timestamp: string }[] = [];
+
+    let count = 0;
+
+    if (checkpointTimes < 1) count = Number(quantity);
+    else count = Number(quantity) - checkpointTimes;
+
+    if (count < 1) {
       return [];
     }
 
-    for (let i = 0; i < Number(quantity); i++) {
+    for (let i = 0; i < count; i++) {
+      const checkpointTime = Math.max(fromTime, startTime);
+      const timestamp = new Date(checkpointTime + (i + 1) * 1000).toISOString();
+
+      const isExpired =
+        new Date(toDate).getTime() < new Date(timestamp).getTime();
+      if (isExpired) break;
+
       res.push({
         completedTimes: 1,
-        timestamp: new Date(
-          nowDate - (Number(quantity) - i) * 1000,
-        ).toISOString(),
+        timestamp,
       });
     }
 
